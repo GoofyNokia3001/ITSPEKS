b685a7404e9359413493a73a5d5b392c7fdc8999
KlimLapTop <ixibiuz@gmail.com>
2024-05-17 11:58:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT Spēks</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">IT - SPĒKS</a>
+        <nav class="navbar">
+            <a href="#">Sākumlapaspidars</a>
+            <a href="#specialitates">Uzņemšana</a>
+            <a href="#informacija">Par mums</a>
+            <a href="#iepazisti">Iepazīsti tehnikumu</a>
+            <a href="#kontakti">Kontakti</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </header>
+    <section id="informacija">
+        <h1><span>PICK "Liepājas Valsts tehnikums"</span></h1>
+        <div class="box-container">
+            <img src="https://t3.ftcdn.net/jpg/06/04/28/14/360_F_604281453_6tg2aIRD86oMv2WTSK12fqAcFCqpQ8hC.jpg">
+            <div class="text-info">
+                <h2>Kurzemes reģiona lielākā profesionālās izglītības iestāde</h2>
+                <p>PIKC "Liepājas Valsts tehnikums" ir Kurzemes reģiona lielākā profesionālās izglītības iestāde ar plašāko piedāvāto izglītības programmu klāstu. Tas nodrošina kompetentu un kvalitatīvu speciālistu sagatavošanu gan pēc pamatskolas, gan pēc vidusskolas beigšanas un arī mūžizglītības kontekstā, atbilstoši reģiona attīstības vajadzībām un dinamikai. Tehnikums ir elastīgs un atvērts moderno tehnoloģiju un jaunu programmu ieviešanai.</p>
+                <div class="facts">
+                    <div class="box">
+                        <i class="fa fa-award"></i>
+                        <b>Vairāk nekā 25 specialitātes</b>
+                    </div>
+                    <div class="box">
+                        <i class="fa fa-user"></i>
+                        <b>1200+ audzēkņu</b>
+                    </div>
+                    <div class="box">
+                        <i class="fa-solid fa-diagram-project"></i>
+                        <b>Neskaitāmi sadarbības partneri</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
