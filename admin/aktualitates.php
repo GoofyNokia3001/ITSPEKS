76c5f4e19b4687fa0616055a70b0727da58a7eff
KlimLapTop <ixibiuz@gmail.com>
2024-05-23 18:54:28 +0300

    Admin tabulas ir noformeti. Samainits stils

--- admin/aktualitates.php
+++ admin/aktualitates.php
@@ -27,56 +27,63 @@
                     <td><img src="https://www.loungelizard.com/wp-content/uploads/top-web-development-companies-2024-1-767x447.webp"></td>
                     <td><h2>TOP Web Development Companies in 2024</h2></td>
                     <td>18/05/2024</td>
-                    <td>It doesn’t matter if you are planning a new website or upgrading an existing site, you want to work with the best web design company available — but how do you find the premier firm with just the right mix of creativity, experience, services, and technology, marketing, and graphic design knowledge? Outstanding design can mean the difference between a lackluster site and one that really pops and attracts attention from the type of customers you want to capture. Websites that are designed and developed by creative, tech forward design companies are better positioned to perform well in the high stakes online and digital marketing game. In fact, 94% of users leave websites with low-quality design and over 85% of online shoppers won’t return to an ecommerce site if they have a bad user experience (UX). Over the last few years, website design and development, especially for mobile devices, have become paramount in importance for companies who want to succeed in a vastly competitive marketplace. We’ve taken a look at the best web design companies doing business today and ranked the top 15 firms in the United States based on their customer ratings, years in business, services provided, and overall customer philosophies. Before you make a decision on the website design company you want to use, take a look at these firms. Top Web Design Companies With over 1.7 billion websites on the internet, you want to work with a top web design company to have any chance of standing out in the crowd. These 15 top designing companies have reputations for creating new and existing websites that set the trends for the industry and rank well on search engine pages (due to outstanding SEO). First, we take a look at each web design agency and then we can discuss what to look for and expect when engaging a top website design company.</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>KristovskisRaimonds</td>
-                    <td>MilakaisITSkolotajs@iambest.com</td>
-                    <td>Klim</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>LiepaAnna</td>
-                    <td>GitarasVirtuozs@rockon.lv</td>
-                    <td>Zane</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>ZilgalvisMiks</td>
-                    <td>VirtuvesEksperiments@chefmaster.com</td>
-                    <td>Astra</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>VilksJanis</td>
-                    <td>LasisKungs@fishingfanatic.net</td>
-                    <td>Teodors</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>OzolinaLiene</td>
-                    <td>Ciemakukulis@bakerylove.com</td>
-                    <td>Rūta</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>BerzinsMartins</td>
-                    <td>LeopardaPukains@junglefever.com</td>
-                    <td>Dace</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
-                    <td>KuzminsViktors</td>
-                    <td>SpagetiEntuziasts@pastalover.com</td>
-                    <td>Inese</td>
+                    <td><img src="https://www.loungelizard.com/wp-content/uploads/Best-Web-Design-Companies-in-2024-767x447.webp"></td>
+                    <td><h2>TOP Web Development Companies in 2024</h2></td>
+                    <td>18/05/2024</td>
+                    <td>Lololo</td>
                     <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
                     <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
                 </tr>
--- admin/pieteikumi.php
+++ admin/pieteikumi.php
@@ -11,11 +11,76 @@
     require "headerAdmin.php";
 ?>
 <body>
-    <div class="pieteikumiAdmin">
-        <table>
-
-        </table>
-    </div>
+    <section class="admin">
+        <div class="moderatoriAdmin">
+            <table>
+                <tr class="heading">
+                    <th>Lietotājvārds</th>
+                    <th>E-pasts</th>
+                    <th>Admins kurš iedeva atļauju</th>
+                    <th class="thButton">Rediģet</th>
+                    <th class="thButton">Dzēst</th>
+                    
+                </tr>
+                <tr>
+                    <td>RavaldsKristaps</td>
+                    <td>ManPatikOrandzasPrezentacijas@iamnerd.com</td>
+                    <td>Elina</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>KristovskisRaimonds</td>
+                    <td>MilakaisITSkolotajs@iambest.com</td>
+                    <td>Klim</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>LiepaAnna</td>
+                    <td>GitarasVirtuozs@rockon.lv</td>
+                    <td>Zane</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>ZilgalvisMiks</td>
+                    <td>VirtuvesEksperiments@chefmaster.com</td>
+                    <td>Astra</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>VilksJanis</td>
+                    <td>LasisKungs@fishingfanatic.net</td>
+                    <td>Teodors</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>OzolinaLiene</td>
+                    <td>Ciemakukulis@bakerylove.com</td>
+                    <td>Rūta</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>BerzinsMartins</td>
+                    <td>LeopardaPukains@junglefever.com</td>
+                    <td>Dace</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>KuzminsViktors</td>
+                    <td>SpagetiEntuziasts@pastalover.com</td>
+                    <td>Inese</td>
+                    <td><button class="Tbtn"><i class="fas fa-edit"></i></button></td>
+                    <td><button class="Tbtn"><i class="fas fa-trash"></i></button></td>
+                </tr>
+            </table>
+        </div>
+    </section>
     
 </body>
 </html>
